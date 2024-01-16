7eb519bca674613088dd3507bb37d3f0c8ef42c0
Paulo Roberto <123490679+PauloRDev@users.noreply.github.com>
2024-01-16 10:48:34 -0300

    Add files via upload

---
+++ Weather App/js/app.js
@@ -0,0 +1,60 @@
+const cityForm = document.querySelector('[data-js="change-location"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document
+  .querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector('[data-js="city-card"]')  
+let timeImg = document.querySelector('[data-js="time"]')
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+
+
+const showCityCard = () => {
+  if (cityCard.classList.contains('d-none')) {
+    cityCard.classList.remove('d-none')
+  }
+
+}
+
+const fetchCityWeatherInfo = async cityName => {
+  const [{ Key, LocalizedName }] = await getCityData(cityName)
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await 
+    getCityWeather(Key)
+
+    return { LocalizedName, WeatherText, Temperature, IsDayTime, WeatherIcon }
+}
+
+const showCityWeatherInfo = async cityName => {
+  const { LocalizedName, WeatherText, Temperature, IsDayTime, WeatherIcon } =
+    await fetchCityWeatherInfo(cityName)
+
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
+  
+  timeImg.src = IsDayTime ?  './src/day.jpg' : './src/night.jpg'
+  timeIconContainer.innerHTML = timeIcon
+  cityNameContainer.textContent =  LocalizedName
+  cityWeatherContainer.textContent = WeatherText  
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+
+  showCityCard()
+}
+
+const showLocalStorageCity = () => {
+  const city = localStorage.getItem('city')
+  
+  if (city) {
+    showCityWeatherInfo(city)
+  }
+}
+
+const handleCityForm = event => {
+  event.preventDefault()
+
+  const inputValue = event.target.city.value
+  showCityWeatherInfo(inputValue)
+  localStorage.setItem('city', inputValue)
+  cityForm.reset()
+}
+
+cityForm.addEventListener('submit', handleCityForm)
+
+showLocalStorageCity()
